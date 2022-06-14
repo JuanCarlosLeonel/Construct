7e96e37f28acf9bb50b2d851afcd68932920a5db
JuanCarlosLeonel <juancble94@hotmail.com>
2022-06-13 21:12:05 -0300

    signals_and_rolepermissionsOK

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from rolepermissions.decorators import has_permission_decorator
 from .models import Users
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib import auth
 
 @has_permission_decorator('cadastrar_vendedor')
 def cadastrar_vendedor(request):
@@ -18,3 +21,25 @@ def cadastrar_vendedor(request):
     user = Users.objects.create_user(username=email, email=email, password=senha, cargo='V')
 
     return HttpResponse('Conta criada')
+
+
+def login(request):
+  if request.method == "GET":
+    if request.user.is_authenticated:
+      return redirect(reverse('plataforma'))
+    return render(request, 'login.html')
+  elif request.method == "POST":
+    login = request.POST.get('email')
+    senha = request.POST.get('senha')
+
+    user = auth.authenticate(username=login, password=senha)
+    if not user:
+      return HttpResponse('Usuário Inválido')
+
+    auth.login(request, user)
+    return HttpResponse('Usuário logado com sucesso')
+
+
+def logout(request):
+  request.session.flush()
+  return redirect(reverse('login'))
